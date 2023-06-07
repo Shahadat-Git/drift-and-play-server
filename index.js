f2432a935de78a4bf83bbc78e793b5eafa8b7135
Shahadat-Git <noyon560@gmail.com>
2023-06-08 02:48:11 +0600

    mongodb connected on server

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@simple-crud-server.g8zjk15.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        
+
+
+
+        await client.db('admin').command({ ping: 1 })
+        console.log('mongodb successfully connected')
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
+
+
 
 
 app.get('/', (req, res) => {
